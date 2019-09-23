a018f78cb484348a4a2c041924e975ae19fdfde2
Valmir Barbosa <valmir.php@gmail.com>
2019-09-23 05:35:11 -0300

    ci(main): configure action

--- test/unit/mongo.spec.js
+++ test/unit/mongo.spec.js
@@ -1,13 +1,17 @@
 'use strict';
 
-const { test, after } = use('Test/Suite')('Mongo');
+const {test, after} = use('Test/Suite')('Mongo');
+
+const Config = use('Config');
 const User = use('App/Models/User');
 
 after(async () => {
   return User.query().delete();
 });
 
-test('Test Create user', async ({ assert }) => {
+test('Test Create user', async ({assert}) => {
+  console.log('CONFIG====>', Config.get('database.connection'), Config.get('database.mongodb'));
+
   const countBefore = await User.count();
   assert.isNull(countBefore);
 
