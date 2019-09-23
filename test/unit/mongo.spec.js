900009ee39948c48df8d879bc5c7ba14042701db
Valmir Barbosa <valmir.php@gmail.com>
2019-09-23 06:23:57 -0300

    ci(main): configure action

--- test/unit/mongo.spec.js
+++ test/unit/mongo.spec.js
@@ -1,22 +1,32 @@
 'use strict';
 
+async function deleteMongoCollection(collection) {
+  const collections =
+    typeof collection === 'string' ? [collection] : collection;
+  const Database = use('Database');
+  const db = await Database.connect('mongodb');
+  const promises = collections.map(c => db.collection(c).remove());
+  return Promise.all(promises);
+}
+
 const { test, after } = use('Test/Suite')('Mongo');
 
 // const Config = use('Config');
 const User = use('App/Models/User');
 
-after(function(done) {
-  // this.enableTimeouts(false);
-  this.timeout(10000);
-
-  User.query()
-    .delete()
-    .then(() => done())
-    .catch(e => {
-      console.error('ERROR:', e);
-      done();
-    });
+after(() => {
+  return deleteMongoCollection('users');
 });
+
+// after(done => {
+//   User.query()
+//     .delete()
+//     .then(() => done())
+//     .catch(e => {
+//       console.error('ERROR:', e);
+//       done();
+//     });
+// });
 
 test('Test Create user', async ({ assert }) => {
   // console.log(
